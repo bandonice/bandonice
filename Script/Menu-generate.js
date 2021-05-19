50ca3c5b5a812e67e1cd88165f270ab31aa0956e
Cooljean <Cooljean95@ymail.com>
2021-05-19 18:38:50 +0200

    Gjort mer med script, og meny.

--- Script/Menu-generate.js
+++ Script/Menu-generate.js
@@ -1,3 +1,33 @@
 
 
 import menuModuel from './MenyModuel.js'
+
+
+const pizzaMeny = document.querySelector("#pizza-menu");
+
+let htmlTxt = "";
+let htmlTxttwo = "";
+
+menuModuel.gettAllPizza().array.forEach(menu => {
+    htmlTxt += `
+        <article>
+            <div>
+                <section>
+                    <img src="">
+                </section>
+                <section>
+                    <h3>${menu.title}<h3>
+                </section>
+                <section>
+                    <h5>${menu.price}<h5>
+                </section>
+                <section>
+                    <p>${menu.info}<p>
+                </section>
+            <div>
+        <article>
+    `
+
+});
+
+pizzaMeny.innerHTML = htmlTxt;
