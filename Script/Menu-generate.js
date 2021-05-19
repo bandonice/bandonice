0e7f6d504402f74f9d50e74e0e2e03c5a90c6277
Cooljean <Cooljean95@ymail.com>
2021-05-19 17:47:19 +0200

    Begynt på moduler i script

---
+++ Script/Menu-generate.js
@@ -0,0 +1,6 @@
+
+
+import menuModuel from './MenyModuel.js'
+
+
+
---
+++ Script/MenuModuel.js
@@ -0,0 +1,41 @@
+
+
+const menuModuel = ( function(){
+
+const menuPizza = [
+    {title:"", price:22, image:"", info:"", },
+    {title:"", price:22, image:"", info:"", },
+    {title:"", price:22, image:"", info:"", },
+    {title:"", price:22, image:"", info:"", },
+    {title:"", price:22, image:"", info:"", },
+]
+
+const menuDrinks = [
+    {title:"", price:22, image:"", info:"", },
+    {title:"", price:22, image:"", info:"", },
+    {title:"", price:22, image:"", info:"", },
+    {title:"", price:22, image:"", info:"", },
+]
+
+const getAllPizza = () => {
+    return menuPizza;
+}
+
+const getAllDrinks = () => {
+    return menuDrinks;
+}
+
+const getPizzaByTitle = (title) => {
+    return menuPizza.filter(pizzaTittle => pizzaTittle.title === title);
+}
+
+const getDrinksByTitle = (title) => {
+    return menuDrinks.filter(drinksTitle => drinksTitle.title === title);
+}
+
+return {getAllDrinks, getAllPizza, getDrinksByTitle, getPizzaByTitle};
+ 
+}());
+
+export default menuModuel;
+
