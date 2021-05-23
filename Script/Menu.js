ce6772409cd53ff4c283fb618ee662ea9c8c1a79
bandonice <bastian@rausand.no>
2021-05-23 20:17:39 +0200

    meny skal kunne legge til, fjerne og redigere items

--- Script/Menu.js
+++ Script/Menu.js
@@ -24,5 +24,39 @@ function myFunction() {
     var priceInput = document.getElementById("menu-add-price").value;
     var infoInput = document.getElementById("menu-add-info").value;
  
-    ul.innerHTML += `<li class="pizzaItem"><a>${nameInput} <br> Price: ${priceInput} <button>Slett</button></a></li>`;
+    ul.innerHTML += `<li class="pizzaItem"><a>${nameInput} <br> Price: ${priceInput}<button onclick="deletePizza()">Slett</button><button onclick="editTest()">Rediger</button> <br> Info: ${infoInput}</a></li>`;
+    var allSubjectName = document.querySelectorAll(".pizzaItem");
+    for (var index = 0; index <allSubjectName.length; index++){
+    allSubjectName[index].addEventListener("click", function(){
+    this.classList.toggle("active");
+    });
+    allSubjectName[index].querySelector("button").addEventListener("click",
+     function(){
+     this.closest(".pizzaItem").remove();
+    });
+     }
+
+  }
+
+  function deletePizza() {
+    var allSubjectName = document.querySelectorAll(".pizzaItem");
+    for (var index = 0; index <allSubjectName.length; index++){
+    allSubjectName[index].querySelector("button").addEventListener("click",
+     function(){
+     this.closest(".pizzaItem").remove();
+    });
+     }
+  }
+
+  function editTest() {
+    var allSubjectName = document.querySelectorAll(".pizzaItem");
+    title = prompt("Ny tittel: ");
+    price = prompt("Ny pris: ");
+    info = prompt("Ny informasjon");
+    for(var i = 0; i < allSubjectName.length; i++) {
+     allSubjectName[i].addEventListener("click", function() {
+       this.closest(".pizzaItem").innerHTML = `<li class="pizzaItem"><a>${title} <br> Price: ${price}<button onclick="deletePizza()">Slett</button><button onclick="editTest()">Rediger</button> <br> Info: ${info}</a></li>`;
+      });
+    }
+   
   }
