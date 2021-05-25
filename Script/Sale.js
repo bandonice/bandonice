03e331f86dccd336ee55ad38093599eaaa3088cb
Cooljean <Cooljean95@ymail.com>
2021-05-25 19:33:17 +0200

    backtracket sale

--- Script/Sale.js
+++ Script/Sale.js
@@ -1,7 +1,8 @@
 
 
-  function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  function dropDownMeny() {
+   const show = document.getElementById("myDropdown").classList.toggle("show");
+   const myDropDown = document.querySelectorAll(".myDropdown");
 }
     window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
@@ -16,51 +17,4 @@
   }
 }
 
-function myFunction2() {
-    document.getElementById("myDropdowntwo").classList.toggle("show");
-}
-    window.onclick = function(event) {
-  if (!event.target.matches('.dropbtntwo')) {
-    var dropdowns = document.getElementsByClassName("dropdown-contenttwo");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
 
-function myFunction3() {
-    document.getElementById("myDropdown3").classList.toggle("show");
-}
-    window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn3')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content3");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-
-function myFunction4() {
-    document.getElementById("myDropdownfour").classList.toggle("show");
-}
-    window.onclick = function(event) {
-  if (!event.target.matches('.dropbtnfour')) {
-    var dropdowns = document.getElementsByClassName("dropdown-contentfour");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
