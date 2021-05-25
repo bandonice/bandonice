e750057dcdf35d7e7c58d0c09d3ad3dbc48e4950
Cooljean <Cooljean95@ymail.com>
2021-05-25 19:16:28 +0200

    fikset menu bug

--- Script/Menu.js
+++ Script/Menu.js
@@ -24,29 +24,24 @@ function AddPizza() {
   var priceInput = document.getElementById("menu-add-price").value;
   var infoInput = document.getElementById("menu-add-info").value;
 
-  ul.innerHTML += `<li class="pizzaItem"><a>${nameInput} <br> Price: ${priceInput}<button onclick="deletePizza()">Slett</button><button onclick="editTest()">Rediger</button> <br> Info: ${infoInput}</a></li>`;
-  var allSubjectName = document.querySelectorAll(".pizzaItem");
+  ul.innerHTML += `<div class="delpizza"><button>Slett</button><li class="pizzaItem"><a>${nameInput} <br> Price: ${priceInput}<button onclick="editTest()">Rediger</button> <br> Info: ${infoInput}</a></li></div>`;
+  var allSubjectName = document.querySelectorAll(".delpizza");
   for (var index = 0; index <allSubjectName.length; index++){
-  allSubjectName[index].addEventListener("click", function(){
-  this.classList.toggle("active");
-  });
   allSubjectName[index].querySelector("button").addEventListener("click",
    function(){
-   this.closest(".pizzaItem").remove();
+   this.closest(".delpizza").remove();
   });
    }
 
 }
 
-function deletePizza() {
-  var allSubjectName = document.querySelectorAll(".pizzaItem");
+  var allSubjectName = document.querySelectorAll(".delpizza");
   for (var index = 0; index <allSubjectName.length; index++){
   allSubjectName[index].querySelector("button").addEventListener("click",
    function(){
-   this.closest(".pizzaItem").remove();
+   this.closest(".delpizza").remove();
   });
    }
-}
 
 function editTest() {
   var allSubjectName = document.querySelectorAll(".pizzaItem");
@@ -55,7 +50,7 @@ function editTest() {
   info = prompt("Ny informasjon");
   for(var i = 0; i < allSubjectName.length; i++) {
    allSubjectName[i].addEventListener("click", function() {
-     this.closest(".pizzaItem").innerHTML = `<li class="pizzaItem"><a>${title} <br> Price: ${price}<button onclick="deletePizza()">Slett</button><button onclick="editTest()">Rediger</button> <br> Info: ${info}</a></li>`;
+     this.closest(".pizzaItem").innerHTML = `<li class="pizzaItem"><a>${title} <br> Price: ${price}<button onclick="editTest()">Rediger</button> <br> Info: ${info}</a></li>`;
     });
   }
  
