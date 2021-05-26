b1a736dd7184dfde493606f0460ea0b4c85325f4
Cooljean <Cooljean95@ymail.com>
2021-05-26 14:34:33 +0200

    fikset på sale

--- Script/Sale.js
+++ Script/Sale.js
@@ -1,14 +1,35 @@
 
 
-  function dropDownMeny() {
-   const show = document.getElementById("myDropdown").classList.toggle("show");
-   const myDropDown = document.querySelectorAll(".myDropdown");
-}
+    
+
+    const dropdownBtnTime = document.getElementById("dropdown-btn-time").addEventListener("click", function(){
+      document.getElementById("my-dropdown-time").classList.toggle("show");
+    });
+    const dropdownBtnAllRest = document.getElementById("dropdown-btn-all-rest").addEventListener("click", function(){
+      document.getElementById("my-dropdown-all-rest").classList.toggle("show");
+    })
+
+    const dropdownBtnRestOne = document.getElementById("dropdown-btn-rest-one").addEventListener("click", function(){
+      document.getElementById("my-dropdown-rest-one").classList.toggle("show");
+    })
+
+    const dropdownBtnRestTwo = document.getElementById("dropdown-btn-rest-two").addEventListener("click", function(){
+      document.getElementById("my-dropdown-rest-two").classList.toggle("show");
+    })
+
+    const dropdownBtnRestThree = document.getElementById("dropdown-btn-rest-three").addEventListener("click", function(){
+      document.getElementById("my-dropdown-rest-three").classList.toggle("show");
+    })
+
+    const dropdownBtnRestFour = document.getElementById("dropdown-btn-rest-four").addEventListener("click", function(){
+      document.getElementById("my-dropdown-rest-four").classList.toggle("show");
+    })
+
+   
     window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
+    for (var i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
