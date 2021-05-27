25b6af8838cdc094ba3f47bfd8ca27d38552ceda
Cooljean <Cooljean95@ymail.com>
2021-05-27 15:06:16 +0200

    fikset mer på meny

--- Script/Menu.js
+++ Script/Menu.js
@@ -20,13 +20,23 @@ function myFunction() {
 }
 
 function AddPizza() {
-  var ul = document.getElementById("test");
   var nameInput = document.getElementById("menu-add-name").value;
   var priceInput = document.getElementById("menu-add-price").value;
   var infoInput = document.getElementById("menu-add-info").value;
   let addPizza = document.getElementById("add-pizza");
 
-  addPizza.innerHTML += `<div class="delete-menu-item"><button>Slett</button><li class="edit-menu-item"><a>${nameInput} <br> Price: ${priceInput}<button onclick="editTest()">Rediger</button> <br> Info: ${infoInput}</a></li></div>`;
+  addPizza.innerHTML += `
+  <div class="delete-menu-item">
+      <button class="added-delete-btn">Delete</button>
+      <li class="edit-menu-item">
+        <img src="image/pizzaEmoji-removebg-preview.png" alt="image" >
+        <h2>${nameInput}</h2>  
+      <button onclick="editFunction()">Edit</button>
+      <h3>Price: ${priceInput}</h3>
+      <a> ${infoInput}</a>
+      </li>
+    </div>
+      `;
   var allSubjectName = document.querySelectorAll(".delete-menu-item");
   for (var index = 0; index <allSubjectName.length; index++){
   allSubjectName[index].querySelector("button").addEventListener("click",
@@ -43,7 +53,18 @@ function AddDrink() {
   var infoInput = document.getElementById("menu-add-info").value;
   let addDrink = document.getElementById("add-drinks");
 
-  addDrink.innerHTML += `<div class="delete-menu-item"><button>Slett</button><li class="edit-menu-item"><a>${nameInput} <br> Price: ${priceInput}<button onclick="editTest()">Rediger</button> <br> Info: ${infoInput}</a></li></div>`;
+  addDrink.innerHTML += `
+  <div class="delete-menu-item">
+      <button class="added-delete-btn">Delete</button>
+      <li class="edit-menu-item">
+        <img src="image/sodaemojijpg-removebg-preview.png" alt="image" >
+        <h2>${nameInput}</h2>  
+      <button onclick="editFunction()">Edit</button>
+      <h3>Price: ${priceInput}</h3>
+      <a> ${infoInput}</a>
+      </li>
+    </div>
+      `;
   var allSubjectName = document.querySelectorAll(".delete-menu-item");
   for (var index = 0; index <allSubjectName.length; index++){
   allSubjectName[index].querySelector("button").addEventListener("click",
@@ -61,15 +82,48 @@ function AddDrink() {
   });
    }
 
-function editTest() {
-  var allSubjectName = document.querySelectorAll(".edit-menu-item");
+function editFunction() {
+  
+  let Image = prompt("Type inn drink or pizza. depending on what you are editing.").toLowerCase();
+
+  if(Image === "pizza"){
+    var allSubjectName = document.querySelectorAll(".edit-menu-item");
   title = prompt("Ny tittel: ");
   price = prompt("Ny pris: ");
   info = prompt("Ny informasjon");
-  for(var i = 0; i < allSubjectName.length; i++) {
+
+    for(var i = 0; i < allSubjectName.length; i++) {
    allSubjectName[i].addEventListener("click", function() {
-     this.closest(".edit-menu-item").innerHTML = `<li class="edit-menu-item"><a>${title} <br> Price: ${price}<button onclick="editTest()">Rediger</button> <br> Info: ${info}</a></li>`;
+     this.closest(".edit-menu-item").innerHTML = `
+     <li class="edit-menu-item">
+        <img src="image/pizzaEmoji-removebg-preview.png" alt="image">
+        <h2>${title}</h2>  
+      <button onclick="editFunction()">Edit</button>
+      <h3>Price: ${price}</h3>
+      <a> ${info}</a>
+      </li>`;
     });
+  }
+  }else if(Image === "drink"){
+    var allSubjectName = document.querySelectorAll(".edit-menu-item");
+  title = prompt("Ny tittel: ");
+  price = prompt("Ny pris: ");
+  info = prompt("Ny informasjon");
+
+  for(var i = 0; i < allSubjectName.length; i++) {
+    allSubjectName[i].addEventListener("click", function() {
+      this.closest(".edit-menu-item").innerHTML = `
+      <li class="edit-menu-item">
+         <img src="image/sodaemojijpg-removebg-preview.png" alt="image">
+         <h2>${title}</h2>  
+       <button onclick="editFunction()">Edit</button>
+       <h3>Price: ${price}</h3>
+       <a> ${info}</a>
+       </li>`;
+        });
+   }
+  }else{
+    alert("you need to type in drink or pizza. Try again.");
   }
  
 }
