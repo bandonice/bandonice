d12367ed05eb365db1e67159571f3f524ba1c062
Buga-00 <cemal.buga@hotmail.com>
2021-05-28 15:25:57 +0200

    Endret på resturant navn på resturantModuel.js

--- Script/ResturantModuel.js
+++ Script/ResturantModuel.js
@@ -2,10 +2,10 @@
 const resturantModuel = ( function(){
 
     const resturants = [
-        {image:"placeholderrest.jpg", name:"Golden Pizza", adresse:"Drammen, olleveien 30", table:12, info:"Denne resturangen ligger ved den nydelige sjøn, som dere kommer til å else. den her plaseringen, stjernene, tur mulighter, hunder, alt man ønsker seg. " },
-        {image:"placeholderrest.jpg", name:"Silver Pizza", adresse:"Asker, kyllning gata 15", table:9, info:"Denne resturangen ligger ved den nydelige sjøn, som dere kommer til å else. den her plaseringen, stjernene, tur mulighter, hunder, alt man ønsker seg. " },
-        {image:"placeholderrest.jpg", name:"Bronce Pizza", adresse:"Oslo, Henrik olsen 46", table:45, info: "Denne resturangen ligger ved den nydelige sjøn, som dere kommer til å else. den her plaseringen, stjernene, tur mulighter, hunder, alt man ønsker seg. "},
-        {image:"placeholderrest.jpg", name:"Platinum Pizza", adresse:"Halden, Evald theies vei 22", table:21, info:"Denne resturangen ligger ved den nydelige sjøn, som dere kommer til å else. den her plaseringen, stjernene, tur mulighter, hunder, alt man ønsker seg. "},
+        {image:"placeholderrest.jpg", name:"Gyldne Pizza Frogner", adresse:"Oslo, Niels Juels gate 33", table:12, info:"Denne resturangen ligger på Frogner. En familievennlig resturant med fantastiske pizzaer. " },
+        {image:"placeholderrest.jpg", name:"Gyldne Pizza Storo", adresse:"Oslo, Vitaminveien 15", table:9, info:"Denne resturangen ligger på Storo med fantastiske pizzaer du aldri har smakt før. " },
+        {image:"placeholderrest.jpg", name:"Gyldne Pizza Grønland", adresse:"Oslo, Grønland 5", table:45, info: "Denne resturangen ligger på Grønland. En fantastisk resturant med en unik pizza meny.. "},
+        {image:"placeholderrest.jpg", name:"Gyldne Pizza Grunerløkka", adresse:"Oslo,  Torggata 21", table:21, info:"Denne resturangen ligger på Grunerløkka. En resturant pefekt for familie og venner. Vi har noen unike og fantastiske pizzaer for en fantastisk pris. "},
         {image:"placeholderrest.jpg", name:"Diamond Pizza", adresse:"kongvinger, Tjuvholmen 9", table:3, info:"Denne resturangen ligger ved den nydelige sjøn, som dere kommer til å else. den her plaseringen, stjernene, tur mulighter, hunder, alt man ønsker seg. " }
     ];
 
