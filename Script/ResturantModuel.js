e4c2cefe99ba1d55d44e563551a6cf195e3486e6
Buga-00 <cemal.buga@hotmail.com>
2021-05-28 15:40:55 +0200

    Endret på info på restaurantene i ResturantModuel.js

--- Script/ResturantModuel.js
+++ Script/ResturantModuel.js
@@ -2,11 +2,10 @@
 const resturantModuel = ( function(){
 
     const resturants = [
-        {image:"placeholderrest.jpg", name:"Gyldne Pizza Frogner", adresse:"Oslo, Niels Juels gate 33", table:12, info:"Denne resturangen ligger på Frogner. En familievennlig resturant med fantastiske pizzaer. " },
-        {image:"placeholderrest.jpg", name:"Gyldne Pizza Storo", adresse:"Oslo, Vitaminveien 15", table:9, info:"Denne resturangen ligger på Storo med fantastiske pizzaer du aldri har smakt før. " },
-        {image:"placeholderrest.jpg", name:"Gyldne Pizza Grønland", adresse:"Oslo, Grønland 5", table:45, info: "Denne resturangen ligger på Grønland. En fantastisk resturant med en unik pizza meny.. "},
-        {image:"placeholderrest.jpg", name:"Gyldne Pizza Grunerløkka", adresse:"Oslo,  Torggata 21", table:21, info:"Denne resturangen ligger på Grunerløkka. En resturant pefekt for familie og venner. Vi har noen unike og fantastiske pizzaer for en fantastisk pris. "},
-        {image:"placeholderrest.jpg", name:"Diamond Pizza", adresse:"kongvinger, Tjuvholmen 9", table:3, info:"Denne resturangen ligger ved den nydelige sjøn, som dere kommer til å else. den her plaseringen, stjernene, tur mulighter, hunder, alt man ønsker seg. " }
+        {image:"placeholderrest.jpg", name:"Gyldne Pizza Frogner", adresse:"Oslo, Niels Juels gate 33", table:12, info:"Denne restauranten ligger på Frogner. En familievennlig restaurant med fantastiske pizzaer som du kommer til å elske.  " },
+        {image:"placeholderrest.jpg", name:"Gyldne Pizza Storo", adresse:"Oslo, Vitaminveien 15", table:9, info:"Denne restauranten ligger på Storo med fantastiske pizzaer du aldri har smakt før. En familievennlig restaurant hvor alle er velkommen. " },
+        {image:"placeholderrest.jpg", name:"Gyldne Pizza Grønland", adresse:"Oslo, Grønland 5", table:45, info: "Denne restauranten ligger på Grønland. En fantastisk familievenlig restaurant med en unik pizza meny som du kommer til å falle for. "},
+        {image:"placeholderrest.jpg", name:"Gyldne Pizza Grunerløkka", adresse:"Oslo,  Torggata 21", table:21, info:"Denne restauranten ligger på Grunerløkka. En restaurant pefekt for familie og venner. Vi har unike og fantastiske pizzaer. "},
     ];
 
     const getAllresturants = () => {
