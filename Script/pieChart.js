dbe6e79da735fa6f4d9acd7bd50ae6bdb0d56e75
bandonice <bastian@rausand.no>
2021-05-31 15:57:20 +0200

    lagt inn kommentarer på sale.html samt redigert litt på sale.css

--- Script/pieChart.js
+++ Script/pieChart.js
@@ -1,3 +1,4 @@
+/*Loads the piechart on entering the sale site. This particular graph was found on the CanvasJS website*/
 window.onload = function() {
     var GyldnePizzaStoro = parseInt(document.getElementById("omsetningEtterSkatt").innerHTML);
     var GyldnePizzaGronnland = parseInt(document.getElementById("omsetningEtterSkatt1").innerHTML);
@@ -30,3 +31,37 @@ window.onload = function() {
   
      chart.render();
       }
+
+/*If the screen is resized, the graph will reload to fit the screen */
+window.onresize = function() {
+  var GyldnePizzaStoro = parseInt(document.getElementById("omsetningEtterSkatt").innerHTML);
+  var GyldnePizzaGronnland = parseInt(document.getElementById("omsetningEtterSkatt1").innerHTML);
+  var GyldnePizzaGrunerlokka = parseInt(document.getElementById("omsetningEtterSkatt2").innerHTML);
+  var GyldnePizzaFrogner = parseInt(document.getElementById("omsetningEtterSkatt3").innerHTML);
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      backgroundColor: "#30363d",
+      
+      title: {
+        text: "Omsetning per restaurant 2021",
+        fontColor: "white",
+      },
+      
+      data: [{
+        type: "pie",
+        startAngle: 240,
+        yValueFormatString: "#### kr",
+        indexLabel: "{label} {y}",
+        indexLabelFontColor: "white",
+        dataPoints: [
+          {y: GyldnePizzaStoro, label: "Gyldne Pizza Storo"},
+          {y: GyldnePizzaGronnland, label: "Gyldne Pizza Grønnland"},
+          {y: GyldnePizzaGrunerlokka, label: "Gyldne Pizza Grünerløkka"},
+          {y: GyldnePizzaFrogner, label: "Gyldne Pizza Frogner"},
+        ]
+      }]
+    });
+
+   chart.render();
+    }
